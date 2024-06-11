03358d2a973df22affe7e53aa4eb4267c695813a
Anartz Mugika Ledo <amugika@atsistemas.com>
2024-06-11 05:49:42 +0200

    feat: upgrade to 9 version in nestjs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
   const config = new DocumentBuilder()
     .setContact(
       'Anartz Mugika',
